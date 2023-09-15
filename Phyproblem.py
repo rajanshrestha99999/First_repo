33c90bb854703a244b6ef19582d401a425771b5d
Rajan Shrestha <rajanshrestha99999@gmail.com>
2023-09-15 20:31:03 +0545

    first code

--- Phyproblem.py
+++ Phyproblem.py
@@ -1,1 +1,9 @@
-# add two numbers
+# Input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is: {sum}")
